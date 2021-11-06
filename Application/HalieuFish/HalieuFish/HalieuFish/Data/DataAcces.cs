0493e6fee791c2f296f26cef521511a7f4aa0acf
Hetrada <Baptistelerate@gmail.com>
2021-11-06 10:36:24 +0100

    init connection sql server

---
+++ Application/HalieuFish/HalieuFish/HalieuFish/Data/DataAcces.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace HalieuFish.Data
+{
+    public class DataAcces:DbContext
+    {
+        public readonly IDbConn dbConn;
+        public DataAcces()
+        {
+            dbConn = new DbConn
+            {
+                server_dbname = "HalieuFishBDD",
+                server_name = "192.168.51.67",
+                server_pass = "AdminPass",
+                server_user = "sa"
+            };
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            ///base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(dbConn.Connection);
+        }
+    }
+}
