574ad0083f90c05bd2dfca67a0b416ce8c303947
Hetrada <Baptistelerate@gmail.com>
2021-11-07 08:46:11 +0100

    ok

--- Application/HalieuFish/HalieuFish/HalieuFish/InscriptionPecheur.xaml.cs
+++ Application/HalieuFish/HalieuFish/HalieuFish/InscriptionPecheur.xaml.cs
@@ -15,11 +15,37 @@ namespace HalieuFish
         public InscriptionPecheur()
         {
             InitializeComponent();
+            string 
+        }
+        private void creerUser(object sender, EventArgs e)
+        {
+            
         }
 
         private void precedent(object sender, EventArgs e)
         {
             App.Current.MainPage = new Page1();
+        }
+
+        public void OnEntryCompletedCE(object sender, EventArgs e)
+        {
+            codeE = ((Entry)sender).Text;
+
+        }
+
+        public void OnEntryCompletedAME(object sender, EventArgs e)
+        {
+            adresseME = ((Entry)sender).Text;
+        }
+
+        private void OnEntryCompletedMDP(object sender, EventArgs e)
+        {
+            mdp = ((Entry)sender).Text;
+        }
+
+        private void OnEntryCompletedNE(object sender, EventArgs e)
+        {
+            nomE = ((Entry)sender).Text;
         }
     }
 }
